db2ec00d5ee28e74bfcdc8ad954fbd67959b7b26
Jake <jakehcasto@aol.com>
2016-05-06 08:11:50 -0400

    Create Main.php

---
+++ src/ImagicalGamer/noVoid/Main.php
@@ -0,0 +1,27 @@
+<?php
+namespace ImagicalGamer\NoVoid;
+    use pocketmine\plugin\PluginBase;
+    use pocketmine\event\Listener;
+    use pocketmine\level\Position;
+    use pocketmine\level\Level;
+    use pocketmine\Player;
+    use pocketmine\entity\Entity;
+    use pocketmine\math\Vector3;
+    use pocketmine\utils\TextFormat as C;
+    use pocketmine\event\player\PlayerMoveEvent;
+    
+class Main extends PluginBase implements Listener{
+    
+public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getLogger()->info(C::GREEN . "Enabled!");
+}
+
+public function checkVoid(PlayerMoveEvent $event){
+    if($event->getTo()->getFloorY() < 0){
+        $player = $event->getPlayer();
+        $player->teleport($this->getServer()->getDefaultLevel()->getSpawn());
+        $player->setHealth($player->getHealth(20));
+         }
+      }
+}
