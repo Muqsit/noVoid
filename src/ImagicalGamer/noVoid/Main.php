f34075a8e7e540e9e601a23c5564b2aa04341f6d
Muqsit Rayyan <hackhack05@gmail.com>
2016-06-21 19:18:33 -0800

    Private

--- src/ImagicalGamer/noVoid/Main.php
+++ src/ImagicalGamer/noVoid/Main.php
@@ -7,25 +7,30 @@ namespace ImagicalGamer\NoVoid;
     use pocketmine\Player;
     use pocketmine\entity\Entity;
     use pocketmine\math\Vector3;
-    use pocketmine\utils\TextFormat as C;
     use pocketmine\event\player\PlayerMoveEvent;
     
 class Main extends PluginBase implements Listener{
     
 public function onEnable(){
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    $this->getLogger()->info(C::GREEN . "Enabled!");
 }
 
 public function checkVoid(PlayerMoveEvent $event){
-    if($event->getTo()->getFloorY() < 0){
-        $player = $event->getPlayer();
-        $x = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getFloorX();
-        $y = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getFloorY();
-        $z = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getFloorZ();
-        $level = $this->getServer()->getDefaultLevel();
-        $player->teleport(new Position($x, $y, $z, $level));
-        $player->setHealth($player->getHealth(20));
+    $player = $event->getPlayer();
+    $x = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getFloorX();
+    $y = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getFloorY();
+    $z = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getFloorZ();
+    $level = $this->getServer()->getDefaultLevel();
+        if($event->getTo()->getFloorY() < 0){
+            switch(mt_rand(1, 2) == 1){
+              case 1:
+              $player->teleport(new Position($x, $y, $z, $level));
+              $player->setHealth($player->getHealth(20));
+              $player->sendTip("§d§l»§r§aYou were saved from the §eVOID §d§l«");
+              break;
+              case 2:
+              break;
+             }
          }
       }
 }
